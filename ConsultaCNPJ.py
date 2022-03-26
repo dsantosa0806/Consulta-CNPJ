1c2afe2f2ae7959bcf8a139485c20c54373c7b4d
Daniel Almeida <93289530+dsantosa0806@users.noreply.github.com>
2022-03-25 22:42:00 -0300

    Update

--- ConsultaCNPJ.py
+++ ConsultaCNPJ.py
@@ -2,6 +2,35 @@ import json
 import requests
 import pandas as pd
 
+def formata_cnpj(numeroCnpj):
+    
+    # Formata o Cnpj com pontos e barras
+    cnpj = '{}.{}.{}/{}-{}'.format(numeroCnpj[:2], numeroCnpj[2:5], numeroCnpj[5:8], numeroCnpj[8:12], numeroCnpj[12:])
+
+    return cnpj
+
+def tratamento_Matriz_Filial(indicadorMatrizFilial):
+    ## Tratamento de Matriz / Filial
+    if indicadorMatrizFilial == 1:
+        return 'MATRIZ'
+    else:
+        return 'FILIAL'
+
+def tratamento_Situacao_Cadastral_Motivo(situacaoCadastralMotivo):
+    ## Tratamento de Situação cadastral sem motivo
+    if situacaoCadastralMotivo == 'SEM MOTIVO':
+        return ''
+    else:
+        return situacaoCadastralMotivo
+
+def tratamento_Ente_Federado(enteFederado):
+
+    ## Tratamento de Ente federado
+    if enteFederado != '':
+        return 'PJ PÚBLICO'
+    else:
+        return 'PJ PRIVADO'   
+
 def get_cnpj_data(cnpj):
 
     # Dado um CNPJ, faz uma requisição para a API Minha Receita. Caso a requisição seja bem sucedida, retorna o conteúdo da requisição em formato json
@@ -23,29 +52,12 @@ for i, cnpj in enumerate(table['CNPJ1']):
     resultado = get_cnpj_data(cnpj)
     razaoSociao = resultado['razao_social']
     situacaoCadastral = resultado['descricao_situacao_cadastral']
-    indicadorMatrizFilial = resultado['identificador_matriz_filial']
-    situacaoCadastralMotivo = resultado['descricao_motivo_situacao_cadastral']
-    enteFederado = resultado['ente_federativo_responsavel']
-    numeroCnpj = resultado['cnpj']
-    numeroCnpjFormatado = '{}.{}.{}/{}-{}'.format(numeroCnpj[:2], numeroCnpj[2:5], numeroCnpj[5:8], numeroCnpj[8:12], numeroCnpj[12:])
-
-## Tratamento de Matriz / Filial
-    if indicadorMatrizFilial == 1:
-        indicadorMatrizFilial = 'MATRIZ'
-    else:
-        indicadorMatrizFilial = 'FILIAL'
-## Tratamento de Situação cadastral sem motivo
-    if situacaoCadastralMotivo == 'SEM MOTIVO':
-        situacaoCadastralMotivo = ''
-    else:
-        situacaoCadastralMotivo
-## Tratamento de Ente federado
-    if enteFederado != '':
-        enteFederado = 'PJ PÚBLICO'
-    else:
-        enteFederado = 'PJ PRIVADO'        
-
-    print(f'''{numeroCnpjFormatado} - ({enteFederado}) - {indicadorMatrizFilial} - {situacaoCadastral} - {situacaoCadastralMotivo} - {razaoSociao} ''')
+    matrizFilial = tratamento_Matriz_Filial(resultado['identificador_matriz_filial'])
+    situacaoCadastralMotivo = tratamento_Situacao_Cadastral_Motivo(resultado['descricao_motivo_situacao_cadastral'])
+    enteFederado = tratamento_Ente_Federado(resultado['ente_federativo_responsavel'])
+    numeroCnpjFormatado = formata_cnpj(resultado['cnpj'])    
+       
+    print(f'''{numeroCnpjFormatado} - {enteFederado} - {matrizFilial} - {situacaoCadastral} - {situacaoCadastralMotivo} - {razaoSociao} ''')
 
 print('consulta finalizada ! ')
 
